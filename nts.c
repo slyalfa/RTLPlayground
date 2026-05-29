6bb876787af20e5acb966e6aca6a7cce56ae375c
forklift <condor@thirdwave.ai>
2026-05-29 16:30:40 -0700

    Flash code work

--- nts.c
+++ nts.c
@@ -1,19 +1,20 @@
 
-//#include <stdint.h>
-//#include "rtl837x_sfr.h"
-//#include "rtl837x_common.h"
-//#include "machine.h"
-//#include "uip/uip.h"
-//#include "uip.h"
-//#include "rtl837x_common.h"
+#include <stdint.h>
+#include "rtl837x_sfr.h"
+#include "rtl837x_common.h"
+#include "machine.h"
+#include "uip/uip.h"
+#include "uip.h"
+#include "rtl837x_common.h"
 
-#pragma codeseg BANK2
-#pragma constseg BANK2
+//#pragma codeseg BANK2
+//#pragma constseg BANK2
 //__xdata uint8_t ntsbuf[TCP_OUTBUF_SIZE];
 
-void nts_init(void) __banked
+void nts_init(void) 
+//void nts_init(void) __banked
 {
-      //  print_string("\nnts_init\n");
+        print_string("\nnts_init\n");
        // uip_listen(HTONS(1234));
 }
 /*
