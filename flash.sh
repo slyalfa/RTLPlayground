1d87b1dc4ee289fd0ab261c0ce837e48032b58a8
forklift <condor@thirdwave.ai>
2026-06-01 12:48:34 -0700

    save rom.layout

--- flash.sh
+++ flash.sh
@@ -8,6 +8,6 @@ stty -F /dev/ttyUSB0 115200 ; sleep 1 ; echo -ne '\x10' > /dev/ttyUSB0
 cd output
 cp rtlplayground.bin paded.bin
 truncate -s 2097152 paded.bin
-flashprog -p serprog:dev=/dev/ttyUSB0:3000000,spispeed=30M --progress --layout rom.layout --image normal -N -w  paded.bin
+flashprog -p serprog:dev=/dev/ttyUSB0:3000000,spispeed=30M --progress --layout ../rom.layout --image normal -N -w  paded.bin
 cd ..
 screen /dev/ttyUSB0 115200
