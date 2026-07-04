e75a4c2a9527bc29f450161a5d9251d4a9ab05d0
Your Name <10572974+eraiza0816@users.noreply.github.com>
2026-07-05 00:51:27 +0900

    support i18n & translate japanese

--- html/navigation.js
+++ html/navigation.js
@@ -1,12 +1,25 @@
 document.getElementById('sidebar').innerHTML =
- "<ul><li><a href='index.html'>Overview</a></li>"
- + "<li><a href='ports.html'>Port Configuration</a></li>"
- + "<li><a href='stat.html'>Port Statistics</a></li>"
- + "<li><a href='vlan.html'>VLAN</a></li>"
- + "<li><a href='l2.html'>L2 Configuration</a></li>"
- + "<li><a href='mirror.html'>Mirroring</a></li>"
- + "<li><a href='lag.html'>Link Aggregation</a></li>"
- + "<li><a href='eee.html'>EEE</a></li>"
- + "<li><a href='bandwidth.html'>Bandwidth Limits</a></li>"
- + "<li><a href='system.html'>System Settings</a></li>"
- + "<li><a href='update.html'>Firmware Update</a></li></ul>";
+ "<ul><li><a href='index.html' data-i18n='nav_overview'>Overview</a></li>"
+ + "<li><a href='ports.html' data-i18n='nav_port_config'>Port Configuration</a></li>"
+ + "<li><a href='stat.html' data-i18n='nav_port_stat'>Port Statistics</a></li>"
+ + "<li><a href='vlan.html' >VLAN</a></li>"
+ + "<li><a href='l2.html' data-i18n='nav_l2'>L2 Configuration</a></li>"
+ + "<li><a href='mirror.html' data-i18n='nav_mirror'>Mirroring</a></li>"
+ + "<li><a href='lag.html' data-i18n='nav_lag'>Link Aggregation</a></li>"
+ + "<li><a href='eee.html' data-i18n='nav_eee'>EEE</a></li>"
+ + "<li><a href='bandwidth.html' data-i18n='nav_bandwidth'>Bandwidth Limits</a></li>"
+ + "<li><a href='system.html' data-i18n='nav_system'>System Settings</a></li>"
+ + "<li><a href='update.html' data-i18n='nav_fw_update'>Firmware Update</a></li></ul>"
+ + "<hr/><div style='padding:8px'><label for='rtl-lang-select' data-i18n='lang_switch'>Language:</label><br/>"
+ + "<select id='rtl-lang-select' onchange='setLang(this.value)' style='width:100%;margin-top:4px'>"
+ + "<option value='en'>English</option><option value='ja'>日本語</option></select></div>";
+
+document.addEventListener('DOMContentLoaded', function() {
+  var sel = document.getElementById('rtl-lang-select');
+  if (sel) sel.value = rtlLang;
+  var links = document.querySelectorAll('#sidebar a[data-i18n]');
+  links.forEach(function(el) {
+    var key = el.getAttribute('data-i18n');
+    if (key) el.textContent = t(key);
+  });
+});
